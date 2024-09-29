884c74ae2d0444b5efd6e26671bb06e26ed9463a
daulet-star <dgochmetov@gmail.com>
2024-09-29 22:37:52 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict={'Daulet ' : 1994,'Katy ' : 2000, 'Karl ' : 2014}
+print(my_dict)
+print(my_dict['Katy '])
+print(my_dict.get('Kira'))
+my_dict.update({'Galy ':1975,'Petra ':1985})
+print(my_dict)
+del my_dict['Daulet ']
+print(my_dict)
+
+my_set={'сироп ',0.5,'nori',34,'сироп ',0.5,'huawei',(34,12),34}
+print(my_set)
+my_set.add('gerls')
+my_set.add(18)
+print(my_set)
+my_set.remove(0.5)
+print(my_set)
